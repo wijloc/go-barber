7d4ca1984681587f08a6cd62804bb5c5a66e2977
Willian Oliveira <wijloc@gmail.com>
2020-11-03 18:35:17 -0300

    build: configure to debug project

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+  const { name, email} = request.body;
+
+  const user = {
+    name,
+    email
+  };
+
+  return response.json(user);
 })
 
 app.listen(3333, () =>{
