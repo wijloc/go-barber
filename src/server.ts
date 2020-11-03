d77890ee3895a5b0cef99922b22444bb5af99434
Willian Oliveira <wijloc@gmail.com>
2020-11-03 19:01:15 -0300

    refactor: separating routes file

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(routes);
 app.use(express.json());
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
