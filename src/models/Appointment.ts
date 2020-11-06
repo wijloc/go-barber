acc6f7514e86da771711eb610d076db5059946d2
Willian Oliveira <wijloc@gmail.com>
2020-11-05 21:23:33 -0300

    fix: rename file name of Appointments class

---
+++ src/models/Appointment.ts
@@ -0,0 +1,22 @@
+import { v4 } from 'uuid';
+
+interface AppointmentsConstructor {
+  provider: string;
+  date: Date;
+}
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    this.id = v4();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
