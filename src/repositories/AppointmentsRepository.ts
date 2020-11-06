cd46e0a8acae53cff250521fd03c402dfb4a375e
Willian Oliveira <wijloc@gmail.com>
2020-11-05 21:03:24 -0300

    refactor: create AppointmentsRepository

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointments = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointments || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
