4d9a03ed7e907f680d40391e2486e79ec01fb563
Willian Oliveira <wijloc@gmail.com>
2020-11-20 17:23:00 -0300

    feat: add repository with datebase

--- src/database/migrations/1604964354249-createAppointments.ts
+++ src/database/migrations/1604964354249-createAppointments.ts
@@ -12,6 +12,7 @@ export default class createAppointments1604964354249
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
