4a960f427dd5566d44df508560f25202eb7f569b
Willian Oliveira <wijloc@gmail.com>
2020-11-23 10:26:37 -0300

    refactor: modify id type of users

--- src/database/migrations/1606134323532-createUsers.ts
+++ src/database/migrations/1606134323532-createUsers.ts
@@ -8,7 +8,7 @@ export default class createUsers1606134323532 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
