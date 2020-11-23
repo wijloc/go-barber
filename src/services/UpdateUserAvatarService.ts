47f1a60c29989ff3faa514a8941307fa2e683bfb
Willian Oliveira <wijloc@gmail.com>
2020-11-23 14:23:59 -0300

    feat: create AppError class

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,8 @@ import { getRepository } from 'typeorm';
 import uploadConfig from '../config/upload';
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   user_id: string;
   avatarFileName: string;
@@ -15,7 +17,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
