0021c326b4c5a98171bda2b05a3c5034605b17df
Willian Oliveira <wijloc@gmail.com>
2020-11-23 09:43:39 -0300

    feat: add created and update date of appointments

--- src/database/migrations/1604964354249-createAppointments.ts
+++ src/database/migrations/1604964354249-createAppointments.ts
@@ -22,6 +22,16 @@ export default class createAppointments1604964354249
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
