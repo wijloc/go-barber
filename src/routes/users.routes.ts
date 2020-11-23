07d4e77f6624c23c82eef2477c877aca2a222870
Willian Oliveira <wijloc@gmail.com>
2020-11-23 11:23:54 -0300

    feat: encrypt password of user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import User from '../models/User';
 
 import CreateUserService from '../services/CreateUserService';
 
@@ -12,7 +13,15 @@ usersRouter.post('/', async (request, response) => {
 
     const user = await createUser.execute({ name, email, password });
 
-    return response.send(user);
+    type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+    type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+    type UserWithoutPassword = PartialBy<User, 'password'>;
+
+    const userWithoutPassword: UserWithoutPassword = { ...user };
+    delete userWithoutPassword.password;
+
+    return response.send(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
